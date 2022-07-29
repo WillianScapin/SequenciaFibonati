d6c3338563206756a50a031c0843c3d35e627d98
willian scapin <willian.scapin@skysoftware.com.br>
2022-07-29 17:41:21 -0300

    Tentando quebrar tudo

--- SequenciaFibonati/Program.cs
+++ SequenciaFibonati/Program.cs
@@ -37,7 +37,7 @@ namespace SequenciaFibonati
             Console.ReadKey();
         }
 
-        // adicionando texto aleatório denovo
+        // Tentando quebrar tudo
 
         public class Valores
         {
